8ea3884714eb2e268daa1dcf79d0638365b8a4ed
davidhansonc <davidhanson.c@gmail.com>
2023-08-27 21:10:06 -0700

    weekly update

--- weekly_update_template.py
+++ weekly_update_template.py
@@ -7,26 +7,26 @@ subject = "Weekly Announcements"
 announcements = f"""
 Hi saints, <br><br>
 
-We have the normal schedule this week. Next week we will start the HWMR from the recent ITERO entitled 
-"Knowing, Experiencing, and Living the All-inclusive Christ for the Genuine Church Life'. The links are updated below if 
-you need to buy a copy. Below there is also an added link to the website for info and sign-up for the Germany gospel trips.
+We have the normal schedule this week. We are beginning week 1 of the new HWMR from the recent ITERO. 
+See the links below if you still need to get a copy.
 
 <br><br>
 Jesus is Lord!
 """
 
-hwmr_week = 5  # update weekly (ascending)
-group_seed = 3 # update weekly (DEscending 6-->1)
+hwmr_week = 1  # update weekly (ascending)
+group_seed = 2 # update weekly (DEscending 6-->1)
 
 
-monday_meeting = f"Life-study of First John (message 19)" # update weekly (ascending)
-thursday_meeting = f"Life-study of Acts (message 72)" # update weekly (ascending)
-friday_meeting = f"Life-study of Hebrews (message 11)" # update weekly (ascending)    
-saturday_meeting = f"Life-study of John (message 22)" #update weekly (ascending)
+monday_meeting = f"Life-study of First John (message 20)" # update weekly (ascending)
+thursday_meeting = f"Life-study of First Peter (message 1)" # update weekly (ascending)
+friday_meeting = f"Life-study of Hebrews (message 12)" # update weekly (ascending)    
+saturday_meeting = f"Life-study of John (message 23)" #update weekly (ascending)
 
 
-HWMR = f"""  
+HWMR = f"""
 HWMR: 
+Knowing, Experiencing, and Living the All-inclusive Christ for the Genuine Church Life
 Making Ourselves Ready for the Lord's Coming
 , week {hwmr_week}
 """
@@ -66,12 +66,10 @@ message_html = f""" <html>
         </ul>
         <i>{HWMR}</i><br>
         If you need a physical copy of the upcoming HWMR book, see the <a href="{HWMR_LSM}">LSM Bookstore</a>.<br>
-        For electronic versions, see the following links:
-        <ul>
-          <li> <a href="{HWMR_kindle}">Kindle</a>
-          <li> <a href="{HWMR_google}">Google Play</a>
-          <li> <a href="{HWMR_apple}">iBook</a>
-        </ul>
+        For electronic versions, see the following links: <a href="{HWMR_kindle}">Kindle</a> | 
+        <a href="{HWMR_google}">Google Play</a> | 
+        <a href="{HWMR_apple}">iBook</a>
+        <br><br>
 
         For the church document with the prophesying, cleaning, and ushering schedules and groups, please use 
         <a href="https://drive.google.com/file/d/1byQPJ73KjIZqgHkgW_U-4MS6fbE5eRQU/view?usp=drive_link">this link</a>.
