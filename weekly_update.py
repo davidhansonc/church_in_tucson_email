e7de13dd177418c5d44d54699b03524e3202c638
David Hanson <davidhanson.c@gmail.com>
2024-08-19 17:12:04 -0700

    update

--- weekly_update.py
+++ weekly_update.py
@@ -5,24 +5,30 @@ from scheduler import get_prophesying_group_order, get_cleaning_group_order#, ge
 
 # Weekly updates
 subject = "Church in Tucson Weekly Announcements"
-hwmr_week = 6  # update weekly
-HWMR = """Knowing, Experiencing, and Enjoying Christ as Revealed in Philippians"""
+hwmr_week = 1  # update weekly
+HWMR = """Being a Vessel unto Honor, a Fully Equipped Man of God, 
+by Being Empowered in the Grace which is in Christ Jesus 
+to Fully Accomplish Our Ministry in the Unique Ministry of God's Economy"""
 HWMR_LSM = "https://www.livingstream.com/en/holy-word-for-morning-revival/99999332-hwmr-being-a-vessel-unto-honor-a-fully-equipped-man-of-god-by-being-empowered-in-the-grace-which-is-in-christ-jesus.html"
-ushers = "David/Jaime"
+ushers = "Jaime/Craig"
 
 # Meeting topics to be updated weekly
-monday_meeting = "Life-study of First Thessalonians (message 12)"
+monday_meeting = "Life-study of First Thessalonians (message 13)"
 thursday_meeting = "Life-study of Second Peter (message 4)"
-friday_meeting = "Life-study of Hebrews (message 51)"
-saturday_meeting = "Life-study of First Timothy (message 9)"
+friday_meeting = "Life-study of Hebrews (message 52)"
+saturday_meeting = "Life-study of First Timothy (message 10)"
 
 announcements = """
 Hi saints, <br><br>
 
-We have the normal meeting schedule this week. We are on the final week of the current Holy 
-Word for Morning Revival (HWMR) book. Next week we will begin the HWMR from the recent international 
-training for elders and resoponsible ones in April. The link below is updated for those who would 
-like to order a copy.
+This week we begin week 1 of the new Holy Word for Morning Revival, linked below.
+<br><br>
+
+Upcoming is the International Thanksgiving Blending Conference on November 28 - December 1, 2024 in Atlanta, Georgia. 
+<a href=https://drive.google.com/file/d/1pEe2ikkHuHtj9qC-9oqgrmjLxnV8qGvK/view?usp=drive_link">Here is a link</a> to the 
+info for the conference. Saints can get a Living Stream Ministry group rate of $119 per night 
+at the Atlanta Marriott Marquis hotel. The number of rooms under this discount is limited, so try to get a room before 
+they run out!
 <br><br>
 
 In Him.
@@ -66,10 +72,12 @@ message_html = f"""<!DOCTYPE html>
         <h2 style="color: #0056b3;">Next Lord's Day ({lords_day_date})</h2>
         
         <p><i>{HWMR}, week {hwmr_week}.</i><br>
-        If you want to buy a physical or electronic copy of the upcoming HWMR book, see the <a href="{HWMR_LSM}" style="color: #0056b3; text-decoration: none;">LSM Bookstore</a>.</p>
+        If you want to buy a physical or electronic copy of the upcoming HWMR book, see the <a href="{HWMR_LSM}" 
+        style="color: #0056b3; text-decoration: none;">LSM Bookstore</a>.</p>
 
         <p>For the church document with the prophesying, cleaning, and ushering schedules and groups, please use 
-        <a href="https://drive.google.com/file/d/15seb5EfFD93_XuVOP4qi5fTzn4wLio8V/view?usp=drive_link" style="color: #0056b3; text-decoration: none;">this link</a>.</p>
+        <a href="https://drive.google.com/file/d/15seb5EfFD93_XuVOP4qi5fTzn4wLio8V/view?usp=drive_link" 
+        style="color: #0056b3; text-decoration: none;">this link</a>.</p>
         
         <h3 style="color: #0056b3;">Prophesying Schedule:</h3>
         <ul style="list-style-type: none; padding: 0;">
@@ -79,8 +87,8 @@ message_html = f"""<!DOCTYPE html>
 		<h3 style="color: #0056b3;">Cleaning Team and Ushers:</h3>
         <p style="margin-bottom: 20px;">{cleaning_formatted}</p>
         <p style="margin-bottom: 20px;"><b>Ushers</b>: {ushers}</p>
-        <p style="margin-bottom: 20px;">Please meet at the hall about a half hour before the Table meeting starts for 
-        cleaning and preparing the elements.</p>
+        <p style="margin-bottom: 20px;">Please meet at the hall about a half hour before the Table meeting starts 
+        for cleaning and preparing the elements.</p>
 
         <h2 style="color: #0056b3; margin-top: 40px;">Meeting Schedule</h2>
         <p>
