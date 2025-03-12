a47a531fb29cab9e9585f60cf60ecb91a8cbbac4
David Hanson <davidhanson.c@gmail.com>
2025-03-11 17:25:03 -0700

    update bfa

--- weekly_update NORMAL.py
+++ weekly_update NORMAL.py
@@ -5,7 +5,7 @@ from scheduler import get_prophesying_group_order, get_cleaning_group_order#, ge
 
 # Weekly updates
 subject = "Church in Tucson Weekly Announcements"
-hwmr_week = 2 # update weekly
+hwmr_week = 1 # update weekly
 HWMR = """Living a Christian Life and Church Life Under the Government of God for the Economy of God"""
 HWMR_link = "https://www.livingstream.com/en/holy-word-for-morning-revival/99999390-hwmr-living-a-christian-life-and-church-life-under-the-government-of-god-for-the-economy-of-god.html"
 min_mag_link = "https://www.livingstream.com/en/ministry-of-the-word/99999388-ministry-of-the-word-periodical-the-vol-29-no-01-february-2025.html"
@@ -21,9 +21,18 @@ announcements = f"""
 Hi saints,
 <br><br>
 
-This coming weekend is the Tucson Book Festival! There will be no meetings this weekend since we will all be at the BFA table. 
-We will send out more information on the book fair later this week. <br><br>
-Next week we will repeat week 2 of the HWMR.
+This week we begin week 1 of the new Holy Word for Morning Revival (HWMR). 
+The links below are all updated for the associated audio messages, the HWMR book, and The Ministry of the Word for the printed messages. 
+If you'd like a physical copy of the HWMR book, you can order your own or see brother Isaac Hsu.
+<br><br>
+
+We are canceling Monday night Life-study meetings for now due to needing that time for a weekly brothers' meeting. 
+The rest of the schedule is to remain the same as normal.
+<br><br>
+
+Don't forget to sign up in the hall for tabling at the book fair! 
+If you do not sign up, you are still welcome to come, but it's helpful for us 
+to have a schedule as much as possible so that we can make sure we have enough saints there for the whole fair.
 
 <br><br>
 In Him.
@@ -64,24 +73,47 @@ message_html = f"""<!DOCTYPE html>
 
         <p style="font-size: 16px;">{announcements}</p>
 
+        <h2 style="color: #0056b3;">Lord's Day, {lords_day_date}</h2>
+        
+        <p><i>{HWMR}, week {hwmr_week}.</i><br><br>
+        For the new Holy Word for Morning Revival book and the associated printed and/or audio 
+        messages from the conference, see the following links:</p>
+        <ul>
+            <li> <a href="{HWMR_link}" style="color: #0056b3; text-decoration: none;">HWMR</a>
+            <li> <a href="{min_mag_link}" style="color: #0056b3; text-decoration: none;">Ministry Magazine</a>
+            <li> <a href="{webcast_link}" style="color: #0056b3; text-decoration: none;">Audio Messages</a>
+        </ul>
+
+        <p>For the church document with the prophesying, cleaning, and ushering schedules and groups, please use 
+        <a href="https://drive.google.com/file/d/15seb5EfFD93_XuVOP4qi5fTzn4wLio8V/view?usp=drive_link" 
+        style="color: #0056b3; text-decoration: none;">this link</a>.</p>
+        
+        <h3 style="color: #0056b3;">Prophesying Schedule:</h3>
+        <ul style="list-style-type: none; padding: 0;">
+            {prophesying_formatted}
+        </ul>
+        Saints generally can focus their speaking on their assigned day, but are not precluded from speaking on a different day
+        if the Spirit inspires you with something. Anyone who is not yet in a group may speak on any day as the Spirit leads.
+        
+		<h3 style="color: #0056b3;">Cleaning Team and Ushers:</h3>
+        <p style="margin-bottom: 20px;">{cleaning_formatted}</p>
+        <p style="margin-bottom: 20px;"><b>Ushers</b>: {ushers}</p>
+        <p style="margin-bottom: 20px;">Please meet at the hall about a half hour before the Table meeting starts 
+        for cleaning and preparing the elements.</p>
+
         <h2 style="color: #0056b3; margin-top: 40px;">Meeting Schedule</h2>
         <p>
             Tuesday: 7:30 PM Prayer Meeting <br>
-            Thursday: {thursday_meeting} <br>
-            Friday: {friday_meeting} <br>
-            Lord's Day Morning: No Meeting <br>
+            Thursday: 7:30 PM {thursday_meeting} <br>
+            Friday: 7:30 PM {friday_meeting} <br>
+            Saturday: 7:30 PM {saturday_meeting} <br>
+            Lord's Day Morning: 10:00 AM Table meeting and 11:00 AM prophesying meeting <br>
         </p>
 
         <h2 style="color: #0056b3; margin-top: 40px;">Upcoming Events</h2>
         <h4>Tucson Festival of Books | March 15-16, 2025</h4>
-        <ul>
-            <li><a href="https://tucsonfestivalofbooks.org/" style="color: #0056b3; text-decoration: none;">tucsonfestivalofbooks.org</a>
-        </ul>
-
-        <h4>International Memorial Day Conference | May 23-26, 2025</h4>
-        <ul>
-            <li>Phoenix, Arizona
-        </ul>
+            <p>Please sign up in the hall for time slot(s) to be at the table. We will also need some to help set up and break down 
+            before and after the festival.</p>
     </div>
 </body>
 </html>
